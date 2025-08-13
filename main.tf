24d6032b0a19ca88db4d1f1fbd4ecccbf5ef73b3
Spandan <SpandanMondal@niteshkushwaha9595fbdgmail.onmicrosoft.com>
2025-08-13 11:19:44 +0530

    add_rg

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.39.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "f96f2e3b-2cc8-4e5e-8ed9-4afb664bb5a8"
+}
+resource "azurerm_resource_group" "rg1" {
+  name     = "coolrg"
+  location = "East US"
+}
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "hoolrg"
+  location = "West Europe"
+}
